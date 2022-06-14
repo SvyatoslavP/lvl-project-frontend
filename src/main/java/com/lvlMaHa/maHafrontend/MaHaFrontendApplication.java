c3a5fc6b14a18e0e493976b369bfa598c2e28711
Panifidkin S.A <@>
2022-06-14 20:19:16 +0700

    start

---
+++ src/main/java/com/lvlMaHa/maHafrontend/MaHaFrontendApplication.java
@@ -0,0 +1,12 @@
+package com.lvlMaHa.maHafrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaHaFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaHaFrontendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lvlMaHa/maHafrontend/MaHaFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvlMaHa.maHafrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaHaFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
