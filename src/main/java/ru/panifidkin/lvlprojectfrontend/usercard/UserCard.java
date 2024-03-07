4ea0e659ff789921edd5d3c4528e095107b3c351
Panifidkin S.A <@>
2024-03-07 12:44:10 +0700

    add password validation

--- src/main/java/ru/panifidkin/lvlprojectfrontend/usercard/UserCard.java
+++ src/main/java/ru/panifidkin/lvlprojectfrontend/usercard/UserCard.java
@@ -1,5 +1,6 @@
 package ru.panifidkin.lvlprojectfrontend.usercard;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
@@ -7,9 +8,13 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Section;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import ru.panifidkin.lvlprojectfrontend.view.MainNavigation;
 
@@ -21,6 +26,9 @@ public class UserCard extends VerticalLayout {
     public static final String PERSONAL_TITLE_ID = "personal-title-id";
     public static final String ACCOUNTING_TITLE_ID = "accounting-title-id";
     public static final String USER_CARD_ID = "user-card-id";
+
+    private Icon checkIcon;
+    private Span passwordStrengthText;
 
     public UserCard() {
         setId(USER_CARD_ID);
@@ -72,14 +80,47 @@ public class UserCard extends VerticalLayout {
         login.setHelperText("Только латинские символы A-Z или числа. От 6 до 20 символов.");
         PasswordField password = new PasswordField("Пароль");
         password.setWidthFull();
-        password.setRequiredIndicatorVisible(true);
         password.setAllowedCharPattern("[A-Za-z0-9]");
         password.setMinLength(6);
         password.setMaxLength(20);
-        password.setHelperText("Только латинские символы A-Z или числа. От 6 до 20 символов.");
+
+        checkIcon = VaadinIcon.CHECK.create();
+        checkIcon.setVisible(false);
+        checkIcon.getStyle().set("color", "var(--lumo-success-color)");
+        password.setSuffixComponent(checkIcon);
+
+        Div passwordStrength = new Div();
+        passwordStrengthText = new Span();
+        passwordStrength.add(new Text("Надежность пароля: "), passwordStrengthText);
+        password.setHelperComponent(passwordStrength);
+
+        password.setValueChangeMode(ValueChangeMode.EAGER);
+        password.addValueChangeListener(e -> {
+            String pass = e.getValue();
+            updateHelper(pass);
+        });
+
+        updateHelper("");
+
         Section accountingInformation = new Section(accountingTitle, login, password);
         accountingInformation.getElement().setAttribute("aria-labelledby", ACCOUNTING_TITLE_ID);
         return accountingInformation;
+    }
+
+    private void updateHelper(String pass) {
+        if (pass.length() > 12) {
+            passwordStrengthText.setText("Надежный");
+            passwordStrengthText.getStyle().set("color", "var(--lumo-success-color)");
+            checkIcon.setVisible(true);
+        } else if (pass.length() > 6) {
+            passwordStrengthText.setText("Не достаточно надежный");
+            passwordStrengthText.getStyle().set("color", "#e7c200");
+            checkIcon.setVisible(false);
+        } else {
+            passwordStrengthText.setText("Не надежный");
+            passwordStrengthText.getStyle().set("color", "var(--lumo-error-color)");
+            checkIcon.setVisible(false);
+        }
     }
 
 }
